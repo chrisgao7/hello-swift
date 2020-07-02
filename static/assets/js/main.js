f8678316ffc47b1b0580ca57acfa9360ed9c32c6
chris <chrisgaoyiping@outlook.com>
2020-07-02 21:44:15 +0800

    修改了导航栏和电脑排版

---
+++ static/assets/js/main.js
@@ -0,0 +1,11 @@
+/* narbar */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 10) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
